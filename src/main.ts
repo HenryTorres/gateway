6c1a9dd67255727b5929ba3420010aefc770f747
HenryTorres <henrytorresg@hotmail.com>
2025-07-06 15:29:54 -0500

    Gateway Products completed - CustomFilterException

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { environmentVars } from './config/envs';
+import { RpcCustomExceptionFilter } from './common/exceptions/rpc-custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(environmentVars.PORT);
 
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,8 @@
-import { BadGatewayException, Controller, Get, Inject } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
+import { ProductCreateDTO } from './dto/product.create.dto';
+import { ProductUpdateDTO } from './dto/product.update.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,10 +10,37 @@ export class ProductsController {
 
   @Get()
   getAll() {
-    return this.productsClient.send({ cmd: 'findAll' }, {})
+    return this.productsClient.send({ cmd: 'products.findAll' }, {})
       .pipe(
         catchError(err => { throw new BadGatewayException(err) })
       );
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'products.findOne' }, { id })
+      .pipe(catchError(err => { throw new BadRequestException(err) }));
+  }
+
+  @Post()
+  create(@Body() dto: ProductCreateDTO) {
+    return this.productsClient.send({ cmd: 'products.create' }, dto)
+      .pipe(catchError(err => { throw new BadRequestException(err) }));
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: ProductUpdateDTO
+  ) {
+    return this.productsClient.send({ cmd: 'products.update' }, { ...dto, id })
+      .pipe(catchError(err => { throw new BadRequestException(err) }))
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'products.delete' }, { id })
+      .pipe(catchError(err => { throw new BadRequestException(err) }))
   }
 
 }
